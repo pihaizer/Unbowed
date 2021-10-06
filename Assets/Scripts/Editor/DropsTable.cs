d743dadca629960a122b113bfdcef58214f68d9f
Vladimir Malhin <malhin.vladimir@gmail.com>
2021-10-06 14:22:10 +0300

    Add drops table

---
+++ Assets/Scripts/Editor/DropsTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using Unbowed.Gameplay.Characters.Configs;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unbowed.Editor {
+    public class DropsTable : OdinEditorWindow {
+        [MenuItem("Tools/Drops table")]
+        static void Open() {
+            var window = GetWindow<DropsTable>();
+            window.Show();
+        }
+
+        protected override void DrawEditor(int index) {
+            base.DrawEditor(index);
+        }
+
+        protected override void OnEnable() {
+            base.OnEnable();
+            drops.Clear();
+            string[] configGUIDs = AssetDatabase.FindAssets("t:CharacterConfig");
+            foreach (string guid in configGUIDs) {
+                var asset = AssetDatabase.LoadAssetAtPath<CharacterConfig>(AssetDatabase.GUIDToAssetPath(guid));
+                drops.Add(new DropRow {name = asset.name, config = asset.dropsConfig});
+            }
+        }
+
+        [SerializeField, Searchable]
+        List<DropRow> drops = new List<DropRow>();
+
+        [Serializable, InlineProperty]
+        class DropRow {
+            [Title("$name")]
+            public DropsConfig config;
+            
+            [ReadOnly, HideLabel]
+            public string name;
+        }
+    }
+}
--- Assets/Scripts/Gameplay/Characters/Configs/CharacterAnimationConfig.cs
+++ Assets/Scripts/Gameplay/Characters/Configs/CharacterAnimationConfig.cs
@@ -6,7 +6,6 @@ namespace Unbowed.Gameplay.Characters.Configs {
     [Serializable]
     [HideLabel]
     [InlineProperty(LabelWidth = 150)]
-    [Title("Animation config")]
     public class CharacterAnimationConfig {
         [Range(0, 1)]
         public float hitMomentPercent;
